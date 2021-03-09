1839e75479cf3084616cc75c8c0205ae0e1611d5
ty <72345833+monty420420@users.noreply.github.com>
2021-03-09 23:25:07 +0900

    m2

--- blog/src/App.js
+++ blog/src/App.js
@@ -2,22 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+   let posts = '강남 고기 맛집';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="black-nav">
+            <div style={ {color : 'blue', fontSize : '30px'} }>개발 blog</div>
+       
+        </div>
+        <img src={logo}/>
+       <h4>{posts}</h4>
     </div>
   );
 }
